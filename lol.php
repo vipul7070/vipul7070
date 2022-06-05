a50af008e9bd2346c8950ed79078526fc2232b06
= <vipulkumar96ara@gmail.com>
2022-06-05 14:07:13 +0530

    added new files

---
+++ lol.php
@@ -0,0 +1,19 @@
+<!doctype html>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title></title>
+	</head>
+	<body>
+		<div>
+			<form accept="" method="POST">
+				NAME: <input type="text" name="fname" placeholder="firstname">
+				<br>
+	
+	            LASTNAME: <input type="text" name="Lname" placeholder="firstname">
+				<br>
+				<input type="submit" name="submit">
+	</body>
+	</html>
